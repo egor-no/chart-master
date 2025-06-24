cd79d4ab40afce6a702fb3317bfb59862f4d9582
egor-no <e.nosko93@ya.ru>
2025-06-24 19:19:57 +0500

    Database connection update

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -8,7 +8,28 @@ public class HibernateUtil {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            return new Configuration().configure("sqlserverMain.cfg.xml").buildSessionFactory();
+            Configuration config = new Configuration().configure("sqlserverMain.cfg.xml");
+
+            // Поддержка Railway
+            String rawUrl = System.getenv("DATABASE_URL");
+            String user = System.getenv("MYSQLUSER");
+            String password = System.getenv("MYSQLPASSWORD");
+
+            if (rawUrl != null && rawUrl.startsWith("mysql://")) {
+                rawUrl = rawUrl.replace("mysql://", "jdbc:mysql://");
+            }
+
+            if (rawUrl != null) {
+                config.setProperty("hibernate.connection.url", rawUrl);
+            }
+            if (user != null) {
+                config.setProperty("hibernate.connection.username", user);
+            }
+            if (password != null) {
+                config.setProperty("hibernate.connection.password", password);
+            }
+
+            return config.buildSessionFactory();
         } catch (Exception e) {
             throw new ExceptionInInitializerError(e);
         }
